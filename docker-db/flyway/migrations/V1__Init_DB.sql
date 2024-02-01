056348e7fe373382dd8cf6c33c294270caf30cf9
Nikita <nikitapolin47@gmail.com>
2024-02-01 15:36:23 +1000

    added docker compose support

--- docker-db/flyway/migrations/V1__Init_DB.sql
+++ docker-db/flyway/migrations/V1__Init_DB.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
                        role       VARCHAR(32)   NOT NULL,
                        first_name VARCHAR(64)   NOT NULL,
                        last_name  VARCHAR(64)   NOT NULL,
-                       loggedIn    BOOLEAN       NOT NULL DEFAULT FALSE,
+                       enabled    BOOLEAN       NOT NULL DEFAULT FALSE,
                        created_at TIMESTAMP,
                        updated_at TIMESTAMP
 );
